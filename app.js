9ef78e38152204705a7a33b387de7a625a3dc283
derek_robertson <dwilliamrobertson@gmail.com>
2016-10-31 10:35:55 -0700

    wired up http for random quote

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 angular.module('quoteApp', [])
   .controller('quoteCtrl', ['$scope', '$http', function($scope, $http){
-    
+    var req = {
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+      method: 'GET',
+      headers: {
+        'X-Mashape-Key': 'TWOBX71fTbmshtrNOFECDRWhVJDAp18Rvf5jsnGdisu1eIzMpF' 
+      }
+    }
+
+    $scope.getQuote = function(){
+      $http(req).then(function success(res){
+        console.log('success', res);
+      }, function error(res){
+        console.log('error', res);
+      })
+    }
   }]);
